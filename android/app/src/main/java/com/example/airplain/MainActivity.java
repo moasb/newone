51e9bb38932a58c6fa66afcde246b9fefd577db1
mosab <flutter1855@gmail.com>
2021-09-08 20:50:26 +0200

    vbksbxkvlfnxlvx

---
+++ android/app/src/main/java/com/example/airplain/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.airplain;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+void main (){
+  runApp(  MaterialApp(home: Scaffold(
+    backgroundColor: Colors.white,
+    body: Column(children: [
+      Image(image: AssetImage('images/airp;ane(1).png'),)
+    ],),
+  ),),);
+
+}
